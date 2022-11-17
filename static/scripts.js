3ae8258a243dcc11c0de91c11283b352f2755bdb
Nicholas Ong <ong.zhengkai.nicholas@dhs.sg>
2022-11-17 21:52:37 +0800

    initial commit

---
+++ static/scripts.js
@@ -0,0 +1,64 @@
+var spending_form = document.getElementById('spendingForm');
+function handleForm(event) {
+    event.preventDefault();
+}
+spending_form.addEventListener('submit', handleForm);
+
+function closeTodo(close) {
+    var span = close.parentElement;
+    span.parentElement.remove();
+    totalCost()
+}
+
+function addItem() {
+    var new_item = document.getElementById('nameInput').value;
+    var new_item_price = document.getElementById('priceInput').value;
+    document.getElementById('nameInput').value = '';
+    document.getElementById('priceInput').value = '';
+
+    var new_item_div = document.createElement('div');
+    new_item_div.appendChild(document.createTextNode(new_item));
+    new_item_div.id = 'itemName';
+
+    var close_icon = document.createElement('span');
+    close_icon.appendChild(document.createTextNode('\u00d7'));
+    close_icon.id = 'close';
+
+    var new_price = document.createElement('span');
+    new_price.appendChild(document.createTextNode(new_item_price));
+    new_price.appendChild(close_icon);
+    new_price.id = 'itemPrice';
+    new_price.className = 'itemPrice';
+
+    var new_item_list = document.createElement('div');
+    new_item_list.id = 'listItem'
+    new_item_list.appendChild(new_item_div);
+    new_item_list.appendChild(new_price);
+
+
+    close_icon.onclick = function () {
+        var span = this.parentElement;
+        span.parentElement.remove();
+        totalCost()
+    };
+
+    var price_list = document.getElementById('spendingList');
+    price_list.appendChild(document.createElement('li').appendChild(new_item_list));
+    totalCost()
+}
+
+
+
+function totalCost() {
+    var allItems = document.getElementsByClassName('itemPrice');
+    var sum = 0
+    for (var i = 0; i < allItems.length; i++) {
+        sum = sum + Number(allItems[i].childNodes[0].textContent);
+    }
+    var totalCost = document.getElementById('totalCost');
+    if (sum === 0) {
+        totalCost.textContent = 0.00;
+    } else {
+        totalCost.textContent = sum
+    }
+}
